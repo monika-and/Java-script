6c604af8645337de00eeaea2e2f0061e03f9a417
monika-and <monica.andrzejewska@gmail.com>
2017-06-10 11:24:09 +0200

    gtablice jako zmienne

---
+++ js/tablice.js
@@ -0,0 +1,16 @@
+'use strict';
+var imiona = new Array();
+var imionaStudentow = new Array(9);
+var imionaChlopakow = new Array('Mateusz', 'Michał', 'Krzysztof', 'Sergio');
+
+imionaStudentow[2] = "Przemek";
+
+console.log(imiona);
+console.log(imionaStudentow);
+console.log(imionaChlopakow);
+
+var wiek = [];
+var wiekStudentow = [20, 20, 20];
+
+console.log(wiek);
+console.log(wiekStudentow);
