fabc72d7178e8bce24bec84ce0721e618f020f60
monika-and <monica.andrzejewska@gmail.com>
2017-06-10 14:35:22 +0200

    operatory

---
+++ js/operatory.js
@@ -0,0 +1,49 @@
+'use strict';
+var liczba = 13, liczba = 5;
+var wynik;
+
+wynik = liczba1 % liczba2;
+conole.log(wynik);
+
+var licz = 2;
+licz += 3;
+//licz =licz + 3;
+console.log(licz);
+
+
+
+
+
+var liczba = "13", liczba = 13;
+if (liczba1 === liczba2) {
+    console.log("równe");
+}
+//sprawdza czy liczby są równe i są równe typy - tu jest tekst "" i liczba - nie jest równe
+
+while (liczba2 > liczba1) {
+    console.log(liczba1)
+    ==liczba1
+}
+
+//nigdy nie używać = w warunkach logicznych bo zawsze będzie true, (przypiszemy wartość)
+
+
+
+var liczba1 = 13, liczba2 = 5; 
+if (liczba2 > 3 && (liczba2 < 10)) {
+    console.log("tak");
+}
+   
+if (!liczba2 > 3 ) {
+    console.log("tak");
+}
+
+
+
+(2>5) ? console.log("tak") : console.log("nie");
+//ten sam zapis tego  warunku
+if (2>5) {
+    console.log("tak")
+} else { 
+    console.log("nie")
+}
