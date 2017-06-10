ebd7da53ec47e5fb8ac6333939532b0f80670b95
monika-and <monica.andrzejewska@gmail.com>
2017-06-10 15:37:35 +0200

    zasiegZmiennych

---
+++ js/zasiegZmiennych.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var krzysiek = 3500;
+var jurek = 5000;
+var jola = 7500;
+var sergio = 2500;
+
+var sumaKasy
+
+function policzSumeKasy(skladowa1, skladowa2, skladowa3, skladowa4)
+{
+    var sumaKasy;
+    sumaKasy = skladowa1 + skladowa2 + skladowa3 + skladowa4;
+    
+    return sumaKasy;
+}
+
+sumaKasy = policzSumeKasy( krzysiek, jurek, jola, sergio);
+
+console.log( sumaKasy );
