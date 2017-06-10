442ea4a3d463872218d39543f0687625bf584212
monika-and <monica.andrzejewska@gmail.com>
2017-06-10 12:35:24 +0200

    conditions

---
+++ js/conditions.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var wzrostMateusza = 170;
+var wzrostOli = 170;
+
+if (wzrostOli < wzrostMateusza) {
+
+    console.log("Ola jest niższa")
+}
+
+
+if (wzrostOli > wzrostMateusza) {
+
+    console.log("Ola jest wyższa");
+} else {
+    console.log("Ola jest niższa");
+}
+
+if (wzrostOli > wzrostMateusza) {
+
+    console.log("Ola jest wyższa");
+    
+} else if (wzrostOli == wzrostMateusza){
+    
+    console.log("Ola tak wysoka jak Mateusz");
+    
+} else { 
+    console.log("Ola jest niższa");
+}
+
+
+var kolor = "zielony";
+switch (kolor) {
+    case "czerwony":
+        console.log("kolor czerwony");
+        break;
+    case "zielony":
+        console.log("kolor zielony");
+      case"niebieski":
+        console.log("kolor niebieski");
+        break;
+    default:
+        console.log("inny kolor");
+             }
